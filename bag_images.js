62b1aa463831be5372f463383a7958f2240900c3
Nishat Naoal <71015522+Nishat-nl@users.noreply.github.com>
2026-04-06 16:44:34 +0700

    Add files via upload

---
+++ bag_images.js
@@ -0,0 +1,7 @@
+const BAG_IMAGES = {
+  bag_1: "assets/bags/bag_1.png",
+  bag_2: "assets/bags/bag_2.png",
+  bag_3: "assets/bags/bag_3.png",
+  bag_4: "assets/bags/bag_4.png",
+  bag_5: "assets/bags/bag_5.png"
+};
