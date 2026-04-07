1a83ca0f15442c8957b1b4ddba7e1017b399d973
Nishat Naoal <71015522+Nishat-nl@users.noreply.github.com>
2026-04-07 16:24:04 +0700

    Add files via upload
    including supabase

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://xfzlgdgskkdmsbhfmxlj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_lTP_SyQDylX7BbzSjAgZWg_JocjnG-z';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
